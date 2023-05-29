6541b389e85de881f1e666321d64b0d8e6793e9c
AllandoAlmeida <alandoalmeida0803@7j50z3.onmicrosoft.com>
2023-05-29 17:09:56 -0300

    Estilização botão navegação herder

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,15 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '192.168.0.11'
+    /* host: '192.168.0.11' */
+    host:'10.0.0.101'
   }
 });
 
+
+
+
+// https://vitejs.dev/config/
+/* export default defineConfig({
+  plugins: [react()],
+}) */
